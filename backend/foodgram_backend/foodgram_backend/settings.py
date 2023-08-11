fb658551bf9bd53012cd16ab280989a1473e9e90
Kirill Chu <kirill@churakov.su>
2023-08-11 09:53:37 +0300

    check1

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='django-insecure-90z@_1tb@v48x#87!s=4=#@(*ek*#0m9pplbz-c20^!(s@)w-^')
+SECRET_KEY = 'django-insecure-90z@_1tb@v48x#87!s=4=#@(*ek*#0m9pplbz-c20^!(s@)w-^'
 
-DEBUG = os.getenv('DEBUG', False).lower() == 'true'
+DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DEBUG', ['*']).split()
+ALLOWED_HOSTS = ['*']
 
 DJANGO_APPS = [
     'django.contrib.admin',
