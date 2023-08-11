cd650e14ce4b469f703c8516e94aa63ac0049119
Kirill Chu <kirill@churakov.su>
2023-08-11 10:15:16 +0300

    check5

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', default=False).lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*']).split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 DJANGO_APPS = [
     'django.contrib.admin',
